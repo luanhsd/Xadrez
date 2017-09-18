a6aaf9b2008eaa2f59af7e455017e6a224f22211
Luan Dantas <luan_h.s.d@hotmail.com>
2017-09-18 12:28:41 -0300

    Criado a classe de posicionamento do Xadrez

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,19 +8,12 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab;
-                tab = new Tabuleiro(8, 8);
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
-                Util.ImprimirTabuleiro(tab);
-            }
-            catch(TabuleiroException e)
-            {
-                Console.Write(e.Message);                
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.ToPosicao());
+
             Console.ReadLine();
         }
     }
---
+++ Xadrez/jogo/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using Xadrez.tabuleiro;
+
+namespace Xadrez.jogo
+{
+    class PosicaoXadrez
+    {
+        public char coluna { get;set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+    }
+}
